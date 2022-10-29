73e5a4575ca7e9fa40275ad402f2523b85a13dd9
brokenarmor451 <bitki3828@gmail.com>
2022-10-29 20:30:47 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell/v2"	
+)
+func main() {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	if err := screen.Init(); err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	
+	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	
+	screen.SetStyle(defStyle)
+	
+	x := 0
+	y := 0
+	for {
+		screen.Sync()
+		switch event := screen.PollEvent().(type) {
+			case *tcell.EventResize:
+			screen.Sync()
+		
+			case *tcell.EventKey:
+			if event.Key() == tcell.KeyCtrlC {
+				screen.Fini()
+				os.Exit(0)
+			}
+			
+			ShowPressedKeys(x + 10  , y + 10, screen, event.Rune())
+			x++
+		}
+	}
+}
+
+func ShowPressedKeys(X, Y int, Screen tcell.Screen, Key rune) {
+	style := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	Screen.SetContent(X, Y / 2 - 4, Key, nil, style)
+}
+
